3d67eb699d0cb23ac6bc0fc6deba5bd4bc118ed0
Evan Marmer <evanmarmer@gmail.com>
2024-02-02 15:03:40 -0700

    fix logged in issue

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,33 @@
 import './Home.css';
 import { Link, Outlet } from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 export default function Home(){
+
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true' ? true : false)
+
+  useEffect(() => {
+    console.log('yahoo')
+    window.location.href = '/explore';
+  }, [])
     
     return (
         <>
-        <h1 className='muse'>Muse</h1>
-        <h2 className='tag'>Frame Your Vision: <br/>Inspiration Awaits</h2>
-        <li>
-          <Link className="SUU" to="/signup">SignUp</Link>
-        </li>
-        <img className="main-img" src="https://i.pinimg.com/originals/3a/bf/ac/3abfacf994ae2eb9e4b92d7c174f7713.jpg" />
-        <div className="glass">
-            <p className="info">Immerse yourself in Muse, where the rich tapestry of 
-            photographers' images unfolds, inviting inspiration for your upcoming field adventure </p>
-        </div>
-
+        { isLoggedIn
+          ? <></>
+          : <>
+            <h1 className='muse'>Muse</h1>
+            <h2 className='tag'>Frame Your Vision: <br/>Inspiration Awaits</h2>
+            <li>
+              <Link className="SUU" to="/signup">SignUp</Link>
+            </li>
+            <img className="main-img" src="https://i.pinimg.com/originals/3a/bf/ac/3abfacf994ae2eb9e4b92d7c174f7713.jpg" />
+            <div className="glass">
+                <p className="info">Immerse yourself in Muse, where the rich tapestry of 
+                photographers' images unfolds, inviting inspiration for your upcoming field adventure </p>
+            </div>
+          </>
+        }
         </>
       )
     }
